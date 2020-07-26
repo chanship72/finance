5775e54ccab0a064c72876e2ac197358976379d8
Chanshin Park <chanshinpark@Chanshins-MacBook-Pro.local>
2020-07-27 06:30:09 +0900

    update news_analysis

---
+++ jobsearch.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from requests import get
+from bs4 import BeautifulSoup
+import os
+import sys
+
+OUTPUT_FILE = "saramin_jd.txt"
+SARAMIN_URL = "http://api.saramin.co.kr/job-search?keywords=%s&count=100&sr=directhire"
+
+def my_print(f, prt):
+	print("{}" .format(prt))
+	f.write("{}\n" .format(prt))
+
+def delete_file():
+    rm_command = "rm %s" % OUTPUT_FILE
+    os.system(rm_command)
+
+def saramin():
+    keywords = ['devops', 'ansible', 'github', '소켓', '패킷', 'docker', 'snmp', 'netconf', 
+	'rest', 'slack', 'jenkins', 'KVM', '오픈스택', 'openstack', 'curl', '오픈소스', 
+	'프로토콜', 'Multithread', 'Multi thread', '멀티스레드', 'Network Programming', 
+	'TCP/IP', '네트워크', 'network']
+	
+    # keywords = ['삼성']
+
+    f = open(OUTPUT_FILE, "a")
+    for keyword in keywords:
+        my_print(f, "#################################################")
+        my_print(f, "#%s" % keyword)
+        my_print(f, "#################################################")
+
+        url = SARAMIN_URL % keyword
+        r = get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        for job in soup.find_all('job'):
+            temp = job.find('company')
+            company = temp.text.strip()
+            my_print(f, "%s, %s, %s" %(company, job.title.text, job.url.text))
+
+        my_print(f, "\n\n")
+
+    f.close()
+
+if __name__ == '__main__':
+    delete_file()
+    saramin()
